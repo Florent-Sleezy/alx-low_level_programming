208a26b7473ae537dfc65029240163c4c3fab535
Florent-Sleezy <florentamenyido@gmail.com>
2023-09-09 12:11:03 +0000

    Printed lowercase alphabet using putchar twice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
