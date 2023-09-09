a09e26e08d84a46c96ad139de66b7526865e52e4
Florent-Sleezy <florentamenyido@gmail.com>
2023-09-09 12:26:56 +0000

    Printed single-digit numbers from 0 to 9 without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
