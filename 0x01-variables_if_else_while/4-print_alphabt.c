93a7a0904ce821aa8db02487af30898715146a4b
Florent-Sleezy <florentamenyido@gmail.com>
2023-09-09 12:19:27 +0000

    Printed alphabet excluding q and e using putchar twice

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
