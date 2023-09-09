6e15d1ac8a9678b4bf8e28bb5843973ea511d964
Florent-Sleezy <florentamenyido@gmail.com>
2023-09-09 12:23:56 +0000

    Printed single-digit numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
