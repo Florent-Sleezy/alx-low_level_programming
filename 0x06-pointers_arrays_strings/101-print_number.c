9280bdaa5413b3814eba5299844735ee6a54aa42
Florent-Sleezy <florentamenyido@gmail.com>
2023-09-22 14:00:41 +0000

    Implemented print_number function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,22 +4,31 @@
  * print_number - Prints an integer
  * @n: The integer to be printed
  */
-void print_number(int n) {
-	if (n < 0) {
+void print_number(int n)
+{
+	if (n < 0)
+	{
 		_putchar('-');
 		n = -n;
 	}
-	
+
+
 	if (n == 0)
 		_putchar('0');
-	
+
+
 	int rev = 0;
-	while (n != 0) {
+
+
+	while (n != 0)
+	{
 		rev = rev * 10 + n % 10;
 		n /= 10;
 	}
-	
-	while (rev != 0) {
+
+
+	while (rev != 0)
+	{
 		_putchar((rev % 10) + '0');
 		rev /= 10;
 	}
